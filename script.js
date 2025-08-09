8d4b9e8c438ae82c2256ae219e1eff1c43e47bbf
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-08-09 14:06:59 -0600

    refactor code while debugging error in singles color blob set

--- script.js
+++ script.js
@@ -16,34 +16,26 @@ let newSelection = {
   shade: '',
 };
 
-let eventStored; //TODO -  Check if needed and delete if not
-
-let clickedEvent = ''; //TODO - Delete after debugging
-
 /***********
  functions 
  ***********/
 
 const markCurrentSelection = function () {
   document.getElementById(currentSelection.group).style =
-    'border: 4px solid black';
+    'border: 4px solid black;';
 
-  document.getElementById(currentSelection.single).style =
-    'border: 4px solid black';
+  document.getElementById(currentSelection.single).style.border =
+    '4px solid black';
 
   document.getElementById(currentSelection.shade).style =
-    'border: 4px solid black';
+    'border: 4px solid black;';
 };
 
-markCurrentSelection();
-
-// DONE
 let clickedItemIsBlob = function (itemsId) {
   if (itemsId === undefined) return 'undefined';
   return itemsId.substring(0, 4) === 'blob';
 };
 
-// DONE
 let getBlobData = function (itemsId) {
   const blobData = {};
   if (itemsId.substring(5, 10) === 'group') {
@@ -61,25 +53,20 @@ let getBlobData = function (itemsId) {
 };
 
 let updateColorPalette = function (blobType, blobColor) {
-  // TODO FIX DEBUG TODO NOTE
-
-  let wasUpdateSuccessful = ''; // TODO - Check if this is still used properly, then decide to  either complete or eliminate
   if (blobType === 'group') updateGroupsPalette(blobColor);
   else if (blobType === 'single') updateSinglesPalette(blobColor, 'listener');
   else if (blobType === 'shade') updateShadesPalette(blobColor);
-  return wasUpdateSuccessful;
 };
 
-//DONE
 const clearCurrentSelection = function () {
-  document.getElementById(currentSelection.group).style =
-    'border: 1px solid black';
+  document.getElementById(currentSelection.group).style.border =
+    '1px solid black';
 
-  document.getElementById(currentSelection.single).style =
-    'border: 1px solid black';
+  document.getElementById(currentSelection.single).style.border =
+    '1px solid black';
 
-  document.getElementById(currentSelection.shade).style =
-    'border: 1px solid black';
+  document.getElementById(currentSelection.shade).style.border =
+    '1px solid black';
 
   // NOTE (a1/2)
   // a red blob with a 'blob-group-0' id is present in the blob-group set,
@@ -89,47 +76,38 @@ const clearCurrentSelection = function () {
   // this problem originates from the fact that only one element can have the
   // 'blob-group-360' id and, therefore, the first blob representing color 360 has the
   // 'blob-group-0' id
-  document.getElementById('blob-group-0').style = 'border: 1px solid black';
+  document.getElementById('blob-group-0').style.border = '1px solid black';
 };
 
-// DONE
 const updateGroupsPalette = function (newGroupColor) {
-  if (newGroupColor === '0') {
-    //NOTE (a2/2) - see note (a1/2) for more detail
-    // a red blob with a 'blob-group-0' id is present in the blob-group set,
-    // this is to allow having a red blob displayed at both ends of the blob-group set.
-    // however, internally, a zero is converted to 360 to simplify the program's logic
-    // and this special case is dealt with as an exeption in this code block
-    newSelection.group = 'blob-group-' + newGroupColor;
-    document.getElementById(newSelection.group).style =
-      'border: 4px solid black';
-    newGroupColor = '360';
-  } else {
-    newSelection.group = 'blob-group-' + newGroupColor;
-    document.getElementById(newSelection.group).style =
-      'border: 4px solid black';
-  }
-
-  // if (blobColor === '0') blobColor = '360';
+  document.getElementById('blob-group-' + newGroupColor).style =
+    'border: 4px solid black;';
   updateSinglesPalette(newGroupColor, 'group');
 };
 
 const updateSinglesPalette = function (newGroupColor, origin) {
   if (origin === 'group') {
     const newSingleColorArray = createNewSingleColorArray(newGroupColor);
-    const oldSingleColorArray = createOldSingleColorArray(newGroupColor);
+    const oldSingleColorArray = createOldSingleColorArray();
 
+    console.log(
+      'updateGroupsPalette() :  >>  oldSingleColorArray, newSingleColorArray',
+      oldSingleColorArray,
+      newSingleColorArray
+    );
+
+    // :DONE:  TODOS  ok hasta aquí...
     updateSinglesPaletteDisplay(oldSingleColorArray, newSingleColorArray);
+
     currentSelection.group = 'blob-group-' + newGroupColor;
     currentSelection.single = 'blob-single-' + newGroupColor;
     currentSelection.shade = 'blob-shade-070040';
   } else if (origin === 'listener') {
-    // FIX - This else may not be needed
+    // :FIX: - This else may not be needed
   }
 };
 
-// DONE
-// REFACTOR > DRY see createOldSingleColorArray
+// :REFACTOR: > DRY see createOldSingleColorArray
 const createNewSingleColorArray = function (newGroupColor) {
   let newSingleColorArray = [];
   let newColorIndex = 0;
@@ -155,14 +133,13 @@ const createNewSingleColorArray = function (newGroupColor) {
   return newSingleColorArray;
 };
 
-// DONE
-// REFACTOR > DRY see createNewSingleColorArray
-const createOldSingleColorArray = function (newGroupColor) {
+// :REFACTOR: > DRY see createNewSingleColorArray
+const createOldSingleColorArray = function () {
   let oldSingleColorArray = [];
   let oldColorIndex = 0;
 
   // NOTE (b2/2)
-  // seame case as indicated in note (b1/2) for createNewSingleColorArray()
+  // same case as indicated in note (b1/2) for createNewSingleColorArray()
 
   let oldBaseColor = Number(getBlobData(currentSelection.group).blobColor);
 
@@ -181,94 +158,125 @@ const createOldSingleColorArray = function (newGroupColor) {
   return oldSingleColorArray;
 };
 
-// DONE
 const updateSinglesPaletteDisplay = function (
   oldSingleColorArray,
   newSingleColorArray
 ) {
-  let oldId = '';
-  let newId = '';
+  // :DEBUG: Vamos aquí...
+  for (let i = 0; i <= 28; i++) {
+    //
+    // change the old id for the new id
+    // on each blob DOM element of the singles palette
+    document
+      .getElementById('blob-single-' + oldSingleColorArray[i])
+      .setAttribute('id', 'blob-single-' + newSingleColorArray[i]);
 
-  // change ids
-  for (let i = 28; i >= 0; i--) {
-    let tempId = ''; // TODO DELETE - debugging code
-    oldId = 'blob-single-' + oldSingleColorArray[i];
-    newId = 'blob-single-' + newSingleColorArray[i];
+    // update the hue of the single color blobs
+    document.getElementById('blob-single-' + newSingleColorArray[i]).style =
+      'background-color: hsl(' + newSingleColorArray[i] + ', 100%, 50%);';
 
-    document.querySelector('#' + oldId).setAttribute('id', newId);
-
-    const blobData = getBlobData(newId);
-
-    //LOG
-    // console.log('updateSinglesPaletteDisplay >  blobData', blobData);
-
-    // update single color blobs display
-    document.getElementById(newId).style =
-      'background-color: hsl(' + blobData.blobColor + ', 100%, 50%);';
-    document.getElementById(newId).textContent = blobData.blobColor;
-    //LOG
-    console.log(
-      'updateSinglesPaletteDisplay >  oldId, newId blobColor',
-      oldId,
-      newId,
-      blobData.blobColor
-    );
-
-    //LOG
-    // console.log(
-    //   'updateSinglesPaletteDisplay > oldId, clickedEvent, newId:::',
-    //   oldId,
-    //   clickedEvent,
-    //   newId
-    // );
+    document.getElementById(
+      'blob-single-' + newSingleColorArray[i]
+    ).textContent = newSingleColorArray[i];
   }
+
+  document.getElementById(
+    'blob-single-' + newSingleColorArray[14]
+  ).style.border = '4px solid black';
+
+  updateShadesPalette(newSingleColorArray[14]);
+
+  document.getElementById('large-blob').style =
+    'background-color: hsl(' + newSingleColorArray[14] + ', 100%, 50%);';
 };
 
-// DONE
 const updateShadesPalette = function (blobColor) {
   return;
 };
 
-/*****************
- event listeners 
- ******************/
-
-/** program's main flow : (01) - start here */
-document.querySelector('body').addEventListener('click', function (event) {
-  //LOG
-  console.log('event listener >  here');
-  const clickedElementsId = event.target.id.toLowerCase();
-  eventStored = event;
-  main(clickedElementsId);
-});
-
-/***************
- program logic
- ***************/
-
-/** program's main flow : (02) - main process starts here */
 let main = function (clickedItemsId) {
-  clickedEvent = clickedItemsId;
-  //LOG
-  console.log('main >  clickedItemsId', clickedItemsId);
   if (clickedItemIsBlob(clickedItemsId)) {
     const blobData = getBlobData(clickedItemsId);
     const blobType = blobData.blobType;
     const blobColor = blobData.blobColor;
 
-    document.querySelector('h3').textContent = clickedEvent; //TODO DELETE - Delete me - Code for debugging
+    document.querySelector('h3').textContent = clickedItemsId; // :TODO: :DELETE: - Delete me - Code used only for debugging purposes
 
     clearCurrentSelection();
 
     updateColorPalette(blobType, blobColor);
-    //LOG
-    console.log('main >  CLICK EVENT PROCESS COMPLETE >>>>>>>>');
-    console.log('');
   }
 };
 
-// REFACTOR LOG DELETE FIX TODO DOING DONE DEBUG NOTE
+/***************
+ program logic
+ ***************/
+
+/*
+   program execution starts here
+
+   by marking the color blobs, corresponding to the 
+   initial color selection, with a black 4px border:
+
+        currentSelection = {
+            group: 'blob-group-285',
+            single: 'blob-single-279',
+            shade: 'blob-shade-070040',
+          };
+          
+*/
+
+markCurrentSelection();
+
+/*
+   then a 'click' event listener is set up
+
+   upon clicking on the screen,
+   main() is called with the clicked element's id
+   as argument, starting the corresponding response   
+*/
+
+/*****************
+ event listeners 
+ ******************/
+
+document.querySelector('body').addEventListener('click', function (event) {
+  //
+  const clickedElementsId = event.target.id.toLowerCase();
+
+  main(clickedElementsId);
+});
+
 //
-// NEXT STEP:
+// :TODO:
 //
+// :FIX: when clicking the 0 blob of the group palette, selecting cusrosr stop working
 // explore what happens with single id when a new group id is chosen
+
+// ****************
+//  HIGHLIGHT TAGS
+// ****************
+
+// :REFACTOR:
+
+// :DELETE:
+
+// :FIX:
+
+// :DEBUG:
+
+// :TODO:
+
+// :DOING:
+
+// :DONE:
+
+// :BUG:
+
+// :NOTE:
+
+// document
+
+// console.log
+
+// background-color
